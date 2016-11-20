3ad4092ad1f192cc25a363df41f6777e271ee87e
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-20 13:35:47 -0700

    Following error occurs. AudioFlinger could not create record track, status: -1. To fix this error please update your build.gradle file on the targetSdkVersion 22 make sure your targetSdkVersion is lower than 22.

--- app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/MainActivity.java
+++ app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rwm = new RecordWavMaster();
+        setUpButtons();
+
     }
 
     private void setUpButtons() {
