d8799adec4979be7dbf9d7972d969b931b2082c7
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-20 18:43:33 -0700

    Added Menu

---
+++ app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.restuarants.smart.speechtotextwatsonv7;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by oscarricaud on 11/20/16.
+ */
+
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
